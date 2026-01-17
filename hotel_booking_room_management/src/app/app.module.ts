53410387a5e72ab1ac34dbad3094d63d080e4b15
San Maria Joby <san.maria@btech.christuniversity.in>
2026-01-17 23:56:05 +0530

    Components Creation

--- hotel_booking_room_management/src/app/app.module.ts
+++ hotel_booking_room_management/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HotelListComponent } from './hotel-list/hotel-list.component';
+import { HotelDetailComponent } from './hotel-detail/hotel-detail.component';
+import { BookingFormComponent } from './booking-form/booking-form.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HotelListComponent,
+    HotelDetailComponent,
+    BookingFormComponent,
+    UserDashboardComponent,
+    AdminPanelComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
